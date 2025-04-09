2a6fde8be03006cc4c30c4b1ee927eedf696779c
Git-Mon930 <subornac860@gmail.com>
2025-04-09 21:06:20 +0900

    GameCore updated

---
+++ src/GameCore/Persistence/Repository/GameRepository.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\GameCore\Persistence\Repository;
+
+use App\GameCore\Persistence\Entity\Game;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Game|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Game|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Game[]    findAll()
+ * @method Game[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class GameRepository extends ServiceEntityRepository
+{
+    const GAME_TIME_RANGE = 'PT180M';
+
+    /**
+     * @param RegistryInterface $registry
+     */
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Game::class);
+    }
+
+    /**
+     * @return Game[]
+     */
+    public function findFutureGames()
+    {
+        $qb = $this->createQueryBuilder('game');
+        $qb->where('game.date > :date')
+            ->setParameter('date', new \DateTime())
+            ->orderBy('game.date', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @return Game[]
+     */
+    public function findPastGames()
+    {
+        $qb = $this->createQueryBuilder('game');
+        $qb->where('game.date <= :date')
+            ->setParameter('date', new \DateTime())
+            ->orderBy('game.date', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @return Game[]
+     */
+    public function findPastGamesById(int $id)
+    {
+        $qb = $this->createQueryBuilder('game');
+        $qb->where('game.date <= :date');
+        $qb->andWhere('game.id = :id');
+        $qb->setParameter('date', new \DateTime());
+        $qb->setParameter('id', $id);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @return Game[]
+     * @throws \Exception
+     */
+    public function findActiveGames()
+    {
+        $dateGameStartRange = new \DateTime();
+        $dateGameStartRange->sub(new \DateInterval(self::GAME_TIME_RANGE));
+
+        $qb = $this->createQueryBuilder('game');
+        $qb->where('game.date <= :dateNow');
+        $qb->andWhere('game.date >= :dateGameStartRange');
+        $qb->setParameter('dateNow', new \DateTime());
+        $qb->setParameter('dateGameStartRange', $dateGameStartRange);
+        $qb->orderBy('game.date', 'ASC');
+
+        return $qb->getQuery()->getResult();
+    }
+}
