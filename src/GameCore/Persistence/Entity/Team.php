7f672626222ca58ffdaf9b02f9fb856ca7b95b4a
Git-Mon930 <subornac860@gmail.com>
2025-04-09 21:06:05 +0900

    GameCore updated

---
+++ src/GameCore/Persistence/Entity/Team.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\GameCore\Persistence\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\GameCore\Persistence\Repository\TeamRepository")
+ */
+class Team
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+}
