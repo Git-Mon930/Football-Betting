4e995e0b78357ac97c01079a2cdee649870cf2c7
Git-Mon930 <subornac860@gmail.com>
2025-04-22 22:13:28 +0900

    Update Games

--- src/GameBetting/Business/Games/UserFutureGames.php
+++ src/GameBetting/Business/Games/UserFutureGames.php
@@ -70,6 +70,7 @@ class UserFutureGames implements UserFutureGamesInterface
             ->findUserBettingByUserId($user, $futureGames);
 
         /** @var UserBettingEntity[] $gameId2UserBets */
+        $gameId2UserBets = [];
         /** @var UserBettingEntity $userBet */
         foreach ($userBets as $userBet) {
             $gameId2UserBets[$userBet->getGame()->getId()] = $userBet;
