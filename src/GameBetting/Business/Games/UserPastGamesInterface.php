fc937ed8a77076bcff88bb5a163e50092d1861c1
Git-Mon930 <subornac860@gmail.com>
2025-04-22 22:12:48 +0900

    Update Games

---
+++ src/GameBetting/Business/Games/UserPastGamesInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GameBetting\Business\Games;
+
+use App\GameBetting\Persistence\DataProvider\GamePastBetting;
+use App\GameCore\Persistence\Entity\Game;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+interface UserPastGamesInterface
+{
+    /**
+     * @param UserInterface $user
+     * @return GamePastBetting[]
+     */
+    public function get(UserInterface $user): array;
+
+
+    /**
+     * @param UserInterface $user
+     * @return GamePastBetting[]
+     */
+    public function getActiveGames(UserInterface $user): array;
+
+    /**
+     * @param UserInterface $user
+     * @param int $id
+     * @return GamePastBetting[]
+     */
+    public function getOnePastGame(UserInterface $user, int $id): array;
+}
